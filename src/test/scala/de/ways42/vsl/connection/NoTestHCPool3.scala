a144cefe4730d3f5eb83b608e915539997b9d501
Stefan Dickas <stefan.dickas@flgruppe.de>
2020-02-19 17:19:26 +0100

    Spiele mit den Task

--- src/test/scala/de/ways42/vsl/connection/NoTestHCPool3.scala
+++ src/test/scala/de/ways42/vsl/connection/NoTestHCPool3.scala
@@ -21,15 +21,15 @@ object NoTestHCPool3    {
   
   def main( args : Array[String]) : Unit = {
     
-//    t1
+    t1
     t2
     t3
-//    t4
+    t4
     t5
     t6
     t7
-//    t8
-//    t9
+    t8
+    t9
     t10
     //Thread.sleep(10000)
     println("awake again")
@@ -53,30 +53,33 @@ object NoTestHCPool3    {
 
   // query
    def t1 =  {
-    	t("t1").runSyncUnsafe(Duration(5000, TimeUnit.SECONDS))
-    	println( "finished: t1" )
+    	val l = t("t1").runSyncUnsafe(Duration(5000, TimeUnit.SECONDS))
+    	println( "finished: t1: " + l.length)
     }  
     
     // keine query
     def t2 =  {
-    	t("t2").foreach{x =>println(x.length);println( "executed-t2: " + x.length)}                                     
+    	t("t2").foreach( x => println( "executed-t2: " + x.length))                                     
     	println( "finished: t2" )
     }
     // keine query
     def t3 =  {
-    	t("t3").runAsync( x => { assert( ! x.contains( Nil)); println( "executed-t3: "/* + x*/)}) 
+    	t("t3").runAsync({ 
+    	  case Right(x) => println( "executed-t3: " + x.length)
+    	  case Left(e)  => println( "executed-t3: " + e)
+    	}) 
     	println( "finished: t3" )
     }
     
     // query
     def t4 =  {
     	val f = t("t4").runToFuture                                       
-    	Await.result( f, Duration(5000, TimeUnit.SECONDS))
-    	println( "finished: t4" )
+    	val l = Await.result( f, Duration(5000, TimeUnit.SECONDS))
+    	println( "finished: t4: " + l.length )
     }
     // keine query
     def t5 =  {
-    	t("t5").runToFuture.foreach{ x => println(x.length);println( "executed-t5: " + x.length)}                         
+    	t("t5").runToFuture.foreach{ x => println( "executed-t5: " + x.length)}                         
     	println( "finished: t5" )
     }
     
@@ -84,12 +87,8 @@ object NoTestHCPool3    {
     def t6 =  {
     	val cancelable = t("t6").runAsync { result =>                   
     	  result match {
-    	    case Right(value) =>
-    	      println(value.length)
-    	      println( "executed-t6: " + value.length)
-    	    case Left(ex) =>
-    	      System.out.println(s"ERROR: ${ex.getMessage}")
-    	      println( "executed-t6: " + ex)
+    	    case Right(value) => println( "executed-t6: " + value.length)
+    	    case Left(ex)     => println( "executed-t6: " + ex)
     	  }
     	}
     	println( "finished: t6" )
@@ -101,25 +100,25 @@ object NoTestHCPool3    {
 	    t("t7").runSyncStep match {                                   
         case Left(future) =>
           // No luck, this Task really wants async execution
-          future.foreach(r => println( "Async: " + r.length))
+          future.foreach(r => println( "executed-t7 async: " + r.length))
         case Right(result) =>
-          println(s"Got lucky: $result.length")
+          println( "executed-t7 sync: " + result.length)
       }
     	println( "finished: t7" )	    
     }
     // query
     def t8 =  {
       import scala.concurrent.duration._                           
-	    Await.result( t("t8").executeAsync.runToFuture, 5000.seconds)
-    	println( "finished: t8" )	    
+	    val l = Await.result( t("t8").executeAsync.runToFuture, 5000.seconds)
+    	println( "finished: t8: " + l.length )	    
     }
     // query mit await nicht mit foreach
     def t9 =  {
 	    import monix.execution.Scheduler                            
 	    import scala.concurrent.duration._
 	    val s = Scheduler.io( "odel")
-	    Await.result(t("t9").executeOn(s).runToFuture, 5.days)
-    	println( "finished: t9" )
+	    val l = Await.result(t("t9").executeOn(s).runToFuture, 5.days)
+    	println( "finished: t9: " +l.length )
     }
     def t10 =  {
       import scala.util.Success
