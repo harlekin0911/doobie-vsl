b32269bfc207385a28d022dcf01ea7d720a5e111
Stefan Dickas <stefan.dickas@flgruppe.de>
2020-07-13 10:52:01 +0200

    JDBC Option bei leeren Queries setzen Parameter in der URL

--- src/test/scala/de/ways42/vsl/domains/vsl/service/TestVslService.scala
+++ src/test/scala/de/ways42/vsl/domains/vsl/service/TestVslService.scala
@@ -19,23 +19,23 @@ import de.ways42.vsl.domains.vsl.service.VslService
 
 class TestVslService  extends AnyFunSuite  { 
   
-  val xa = Connect( "com.ibm.db2.jcc.DB2Driver", "jdbc:db2://172.17.4.39:50001/vslt01", "VSMADM", "together")
+  val xa = Connect( "com.ibm.db2.jcc.DB2Driver", "jdbc:db2://172.17.4.39:50001/vslt01:allowNextOnExhaustedResultSet=1;", "VSMADM", "together")
   
   
-  test( "VS-GetSingleVertrag") {
+  test( "VS-GetSingleVertrag-beides") {
     val (o1,l2) = VslService.getVertragWithVersicherung( "1234567890123")
     val a = o1.transact(xa).unsafeRunSync()
     val b = l2.transact(xa).unsafeRunSync()
     assert(  a.isEmpty == true && b.size == 4)
 
  	}
-  test( "VS-GetSingleVertrag-1") {
+  test( "VS-GetSingleVertrag-1-opt-vertrag") {
     val (o1,l2) = VslService.getVertragWithVersicherung( "1234567890123")
     val a = o1.transact(xa).unsafeRunSync()
     assert(  a.isEmpty == true )
 
  	}
-  test( "VS-GetSingleVertrag-2") {
+  test( "VS-GetSingleVertrag-2-list-vers") {
     val (o1,l2) = VslService.getVertragWithVersicherung( "1234567890123")
     val b = l2.transact(xa).unsafeRunSync()
     assert(  b.size == 0)
